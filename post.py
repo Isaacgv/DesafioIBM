7718a93f229b32b3207a0a676f56164736b756cb
Isaacgv <33132205+Isaacgv@users.noreply.github.com>
2021-12-18 20:01:27 +0100

    Create post.py

---
+++ post.py
@@ -0,0 +1,13 @@
+import json
+
+data= {
+  "email": "isaac.gonzales@alumni.usp.br",
+  "assistantId": "4d5af747-4edd-4147-9689-7badeef8c65a",
+  "url": "https://api.us-south.assistant.watson.cloud.ibm.com/instances/cb500637-e7d0-4d12-ad52-aad24714feee",
+  "skillId": "4d5af747-4edd-4147-9689-7badeef8c65a",
+  "apiKey": "zyoYgnlCGewxlM3fadB6_y3Xr4382BABTaf21H5oRPAh",
+  "submitConfirmation": False
+}
+url = "http://172.21.188.211:3000/submit"
+headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+r = requests.post(url, data=json.dumps(data))
